ca0f6ca66a4e70f01e18ea71230e772af5706384
eric.mailman@yahoo.com <eric.mailman@yahoo.com>
2025-02-02 21:01:48 +0000

    Created OrderForm3 and OrderForm3.RowTemplate1; Edited OrderForm2

--- client_code/OrderForm3/__init__.py
+++ client_code/OrderForm3/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import OrderForm2Template
+from ._anvil_designer import OrderForm3Template
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class OrderForm2(OrderForm2Template):
+class OrderForm3(OrderForm3Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
@@ -13,7 +13,7 @@ class OrderForm2(OrderForm2Template):
     # Any code you write here will run before the form opens.
 
   def button_decr_click(self, **event_args):
-    """ Event handler for the decrease button """
+    """Event handler for the decrease button"""
     # Decrease the quantity, but not less than 0
     qty = int(self.text_qty.text)
     if qty > 0:
@@ -26,13 +26,12 @@ class OrderForm2(OrderForm2Template):
       # Calculate the extended price
       ext_price = float(unit_price) * int(self.text_qty.text)
       print(self.text_qty.text, unit_price, ext_price)
-      
-      # Show the extended price
-      self.text_ext_price.text = f'${ext_price:.2f}'
 
-      
+      # Show the extended price
+      self.text_ext_price.text = f"${ext_price:.2f}"
+
   def button_incr_click(self, **event_args):
-    """ Event handler for the increase button """
+    """Event handler for the increase button"""
     # Increase the quantity, but not more than 10
     qty = int(self.text_qty.text)
     if qty < 10:
@@ -45,6 +44,6 @@ class OrderForm2(OrderForm2Template):
       # Calculate the extended price
       ext_price = float(unit_price) * int(self.text_qty.text)
       print(self.text_qty.text, unit_price, ext_price)
-      
+
       # Show the extended price
-      self.text_ext_price.text = f'${ext_price:.2f}'
+      self.text_ext_price.text = f"${ext_price:.2f}"
