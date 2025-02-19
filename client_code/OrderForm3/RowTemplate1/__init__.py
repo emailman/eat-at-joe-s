271bd5849784aaec91ae7e1436793416e6d22b80
eric.mailman@yahoo.com <eric.mailman@yahoo.com>
2025-02-19 22:14:28 +0000

    Edited OrderForm3.RowTemplate1

--- client_code/OrderForm3/RowTemplate1/__init__.py
+++ client_code/OrderForm3/RowTemplate1/__init__.py
@@ -17,7 +17,7 @@ class RowTemplate1(RowTemplate1Template):
     # Decrease the quantity, but not less than 0
     qty = int(self.text_qty.text)
     if qty > 0:
-      self.text_qty.text = f'{qty - 1:3d}'
+      self.text_qty.text = qty - 1
       self.calc_extended_price()
       
 
