4570dcdf4f2fddc0902f48421346c11e7acfcffc
eric.mailman@yahoo.com <eric.mailman@yahoo.com>
2025-04-01 19:11:45 +0000

    Created Menu.Items; Edited Menu; Deleted Menu.RowTemplate1

--- client_code/Menu/Items/__init__.py
+++ client_code/Menu/Items/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RowTemplate1Template
+from ._anvil_designer import ItemsTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -7,7 +7,7 @@ from anvil.tables import app_tables
 from ...AddItem import AddItem
 
 
-class RowTemplate1(RowTemplate1Template):
+class Items(ItemsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
